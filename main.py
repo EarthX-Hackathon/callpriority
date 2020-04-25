ceac1d59e025a1c1b91f28f3fdea8b6f4d89ec32
Ajit Rajurkar <ajit.rajurkar619@gmail.com>
2020-04-25 17:21:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,18 @@ from flask import Flask
 from twilio.twiml.voice_response import VoiceResponse
 from twilio import twiml
 import flask
+import csv
+import requests
 
 from twilio.rest import Client
 import spacy
-nlp = spacy.load('en')
+nlp = spacy.load('en_core_web_sm')
 from flask import Flask, jsonify
 from flask import request, redirect
 
 from flask import Flask, request, redirect
 from twilio.twiml.messaging_response import MessagingResponse
-from twilio.twiml.voice_response import VoiceResponse, Gather
+from twilio.twiml.voice_response import Record, VoiceResponse, Gather
 
 
 app = Flask(__name__)
@@ -23,8 +25,8 @@ def voice():
 
     g = Gather(num_digits=1, action='/handleVoice')
     g.say("Welcome, press one for English", voice='alice', language="en-US")
-	g.say("Bienvenido, para español, presione 2", voice='alice', language="es-MX")
-	g.say("欢迎，按3为中文", voice='alice', language="zh-CN")
+    g.say("Bienvenido, para español, presione 2", voice='alice', language="es-MX")
+    g.say("欢迎，按3为中文", voice='alice', language="zh-CN")
 
     resp.append(g)
 
@@ -35,63 +37,61 @@ def voice():
 
 @app.route("/handleVoice", methods=['GET', 'POST'])
 def handleVoice():
-	resp = VoiceResponse()
+    resp = VoiceResponse()
 
     if 'Digits' in request.values:
 		# Get which digit the caller chose
-		choice = request.values['Digits']
-		phone = request.values['From']
+        choice = request.values['Digits']
+        phone = request.values['From']
 
         if choice == '1':
-			resp2 = VoiceResponse()
-			resp2.say("Hi, how can I help you today?", voice='alice', language="en-US")
+            resp2 = VoiceResponse()
+            resp2.say("Hi, how can I help you today?", voice='alice', language="en-US")
 
-			resp2.record(timeout=5, transcribe=True, transcribeCallback="/handleVoiceResponse")
-			print(resp2)
-			return str(resp2)
+            resp2.record(maxLength = 20,timeout=10, transcribe=True, transcribeCallback="/handleVoiceResponse")
+            print(resp2)
+            return str(resp2)
 
-		elif choice == '2':
-			resp2 = VoiceResponse()
-			resp2.say("Hola como puedo ayudarte hoy?", voice='alice', language="es-MX")
+        elif choice == '2':
+            resp2 = VoiceResponse()
+            resp2.say("Hola como puedo ayudarte hoy?", voice='alice', language="es-MX")
 
-			resp2.record(timeout=5, transcribe=True, transcribeCallback="/handleVoiceResponse")
-			print(resp2)
-			return str(resp2)
+            resp2.record(timeout=5, transcribe=True, transcribeCallback="/handleVoiceResponse")
+            print(resp2)
+            return str(resp2)
 
-		elif choice == '3':
-			resp2 = VoiceResponse()
-			resp2.say("嗨，我今天怎么能帮到你？", voice='alice', language="zh-CN")
+        elif choice == '3':
+            resp2 = VoiceResponse()
+            resp2.say("嗨，我今天怎么能帮到你？", voice='alice', language="zh-CN")
 
-			resp2.record(timeout=5, transcribe=True, transcribeCallback="/handleVoiceResponse")
-			print(resp2)
-			return str(resp2)
+            resp2.record(maxLength = 20, timeout=5, transcribe=True, transcribeCallback="/handleVoiceResponse")
+            print(resp2)
+            return str(resp2)
 
-		else:
-			resp.say("Sorry, I dint understand the input")
-			return ""
+        else:
+            resp.say("Sorry, I dint understand the input")
+            return ""
 
-
-
-	return ""
+    return ""
 
 
 @app.route("/handleVoiceResponse", methods=['GET', 'POST'])
-def handleVoiceREsponse
+def handleVoiceREsponse():
     resp = VoiceResponse()
     soundURL = (request.values['RecordingUrl'])
     print (request.values)
     calltext = request.values['TranscriptionText']
-    replytext =''
+    replytext ='nothing came'
     gotreply = False
 
-	with open('FAQ.csv', encoding="utf8") as csv_file:
-		csv_reader=csv.reader(csv_file , delimiter=',')
+    with open('FAQ.csv', encoding="utf8") as csv_file:
+        csv_reader=csv.reader(csv_file , delimiter=',')
 
-		for row in csv_reader:
-			similarity = nlp(row[0].lower()).similarity(nlp(calltext.lower()))
-			print (similarity)
-			print (str(calltext), str(row[0]))
-			if similarity > 0.80:
+        for row in csv_reader:
+            similarity = nlp(row[0].lower()).similarity(nlp(calltext.lower()))
+            print (similarity)
+            print (str(calltext), str(row[0]))
+            if similarity > 0.80:
                 gotreply = True
                 replytext = str(row[1])
 
